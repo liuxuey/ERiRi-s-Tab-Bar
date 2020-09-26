0280028885bc5f3c7ee02f52f0a638a296afc8fe
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 18:27:47 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:27:47 up 45 min,  1 user,  load average: 1.63, 1.32, 0.69 5109b35ee10454efa46b21bdf54f98aedc194470

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,7 +37,12 @@ static int cmd_q(char *args) {
 }
 static int cmd_si(char *args)
 {
-	return -1;
+	int n=0;
+	int t;
+	if(args==NULL) n=1;
+	else t=scanf(args,"%d",&n);
+	cpu_exec(n);
+	return 0;
 }
 
 static int cmd_help(char *args);
@@ -50,7 +55,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-	{"si [N]","程序单步执行 N 条指令后暂停,当 N 没有给出时, 缺省为 1。",cmd_si}
+	{"si n","程序单步执行 N 条指令后暂停,当 N 没有给出时, 缺省为 1。",cmd_si}
 	/* TODO: Add more commands */
 
 };
