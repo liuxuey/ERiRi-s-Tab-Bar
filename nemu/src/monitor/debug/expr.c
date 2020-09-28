a2af0bed5f890a01d2064c4209a66a045f03dc26
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 23:59:37 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:59:37 up 31 min,  1 user,  load average: 1.53, 1.11, 0.95 eee894ab6d96e50bf62d3fdc965b1cb70b43067

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -99,7 +99,11 @@ static bool make_token(char *e) {
 					case ')': {tokens[nr_token].type=')'; nr_token++; tokens[nr_token].priority1=100;break;}
 					case EQ: {tokens[nr_token].type=EQ; nr_token++; break;}		//     等于
 					// case 256: {tokens[nr_token].type=256; nr_token++; tokens[nr_token].priority1=100000000;break;}	//case 空格
-					case 258: {printf("here4\n");tokens[nr_token].type=258; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); tokens[nr_token].priority1=10000000;break;}
+					case 258: {printf("here4\n");tokens[nr_token].type=258;
+							   strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len);
+							   printf("here5\n");
+							   tokens[nr_token].priority1=10000000;
+							    break;}
 					default: panic("please implement me");
 				}
 
