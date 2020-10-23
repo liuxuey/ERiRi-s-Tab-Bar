15d7c086dff4df7055f889c360083a66d10cc267
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-24 00:09:39 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:09:39 up  5:31,  1 user,  load average: 1.81, 1.45, 1.28 a2d80801651653e3f526d11dac54d3cd322e2650

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -7,7 +7,7 @@ static void do_execute() {
     
     swaddr_t addr = instr_fetch(cpu.eip + 1, 1);
     printf("here1\n");
-    cpu.eip+=5;
+    
     printf("here1\n");
     cpu.eip+=addr;
     printf("%x\n",cpu.eip);
