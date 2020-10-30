f956bc513ebb68d73b6a2228d80325a8dcc27c93
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 01:17:33 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:17:33 up  3:23,  1 user,  load average: 0.43, 0.46, 0.47 559f601bcc840cc2b50b351bd636a9647e80ec90

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -1,24 +1,20 @@
+
 #include "cpu/exec/template-start.h"
 
 #define instr dec
 
 static void do_execute () {
 	DATA_TYPE result = op_src->val - 1;
-	int len = (DATA_BYTE << 3) - 1;
-	cpu.CF = op_src->val < 1;
-	cpu.SF=result >> len;
-    	int s1,s2;
-	s1=op_src->val>>len;
-	s2=0;
-    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
-	cpu.ZF=!result;
 	OPERAND_W(op_src, result);
-	result ^= result >>4;
-	result ^= result >>2;
-	result ^= result >>1;
-	cpu.PF=!(result & 1);
-	/* TODO: Update EFLAGS. */
-	//panic("please implement me");
+
+	concat(updateCPU_, SUFFIX) (result);
+	cpu.CF = op_src->val < 1;
+	int s1, s2;
+	int len = (DATA_BYTE << 3) - 1;
+	s1 = op_src->val >> len;
+	s2 = 0;
+    cpu.OF = (s1 != s2 && s2 == cpu.SF);
+
 	print_asm_template1();
 }
 
