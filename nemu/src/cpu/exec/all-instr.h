2c2105234333f118597103d91af89474b9dbc5b8
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 16:32:44 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:32:44 up  6:16,  1 user,  load average: 2.09, 1.59, 1.54 72bc22665f0c8286bb406058331d2e3bfb191cf

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -14,6 +14,7 @@
 #include "sete/sete.h"
 #include "stos/stos.h"
 #include "lea/lea.h"
+#include "movsx/movsx.h"
 #include "jg/jg.h"
 #include "jl/jl.h"
 #include "leave/leave.h"
---
+++ nemu/src/cpu/exec/movsx/movsx.c
@@ -0,0 +1,7 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 4
+#include "movsx-template.h"
+#undef DATA_BYTE
+/* for instruction encoding overloading */
+
