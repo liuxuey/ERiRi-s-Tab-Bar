216e10b01cd35dd4ea092b61eee726e5f6cbf3e0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 20:30:22 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:30:22 up 10:13,  1 user,  load average: 0.57, 0.64, 0.81 43c61240ce65e46b9d4adfdbc44ae9436116673f

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -1,4 +1,3 @@
-
 #ifndef __REG_H__
 #define __REG_H__
 
@@ -7,7 +6,27 @@
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum { R_ES, R_CS, R_SS, R_DS, R_FS, R_GS };
 
+struct SREG{
+		uint16_t selector;
+		union {
+			struct {
+				uint32_t seg_base1 :16;
+				uint32_t seg_base2 :8;
+				uint32_t seg_base3 :8;
+			};
+			uint32_t seg_base;
+		};
+		union {
+			struct {
+				uint32_t seg_limit1 :16;
+				uint32_t seg_limit2 :4;
+				uint32_t seg_limit3 :12;
+			};
+			uint32_t seg_limit;
+		};
+};
 
 typedef struct {
 union {
@@ -48,13 +67,100 @@ union {
 	bool INTR;
 };
 	
+struct GDTR{
+		uint32_t base_addr;
+		uint16_t seg_limit;
+}gdtr;
+struct IDTR{
+		uint32_t base_addr;
+		uint16_t seg_limit;
+}idtr;
+	CR0 cr0;
+	CR3 cr3;
+
+union {
+        	struct SREG sr[6];
+        	struct 
+        	{
+            struct SREG es, cs, ss, ds, fs, gs;
+        	};
+};
 
 
 } CPU_state;
 
+typedef  union {
+	struct {
+		uint16_t rpl	:2;
+		uint16_t ti	:1;
+		uint16_t index 	:13;
+	};
+	uint16_t val;
+}SELECTOR;
+typedef struct {
+	union {
+		struct {
+			uint32_t seg_limit1	:16;
+			uint32_t seg_base1	:16;
+		};
+		uint32_t first_part;
+	};
+	union {
+		struct {
+			uint32_t seg_base2 	:8;
+			uint32_t type		:5;
+			uint32_t dpl		:2;
+			uint32_t p		:1;
+			uint32_t seg_limit2	:4;
+			uint32_t avl		:1;
+			uint32_t 		:1;
+			uint32_t b		:1;
+			uint32_t g		:1;
+			uint32_t seg_base3	:8;
+		};
+		uint32_t second_part;
+	};
+}SEG_descriptor;
+typedef struct {
+	union {
+		struct {
+			uint32_t p 	:1;
+			uint32_t rw	:1;
+			uint32_t us	:1;
+			uint32_t 	:2;
+			uint32_t a	:1;
+			uint32_t d 	:1;
+			uint32_t 	:2;
+			uint32_t avail	:3;
+			uint32_t addr 	:20;
+		};
+		uint32_t page_val;
+	};
+}PAGE_descriptor;
+
+typedef struct GateDescriptor {
+	union {
+		struct {
+			uint32_t offset_15_0      : 16;
+			uint32_t segment          : 16;
+			uint32_t pad0             : 8;
+			uint32_t type             : 4;
+			uint32_t system           : 1;
+			uint32_t privilege_level  : 2;
+			uint32_t present          : 1;
+			uint32_t offset_31_16     : 16;
+		};
+		struct {
+		uint32_t first_part;
+		uint32_t second_part;
+		};
+	};
+}GATE_descriptor;
 
 CPU_state cpu;
-
+uint8_t current_sreg;
+SEG_descriptor *seg_des;
+GATE_descriptor *idt_des;
 
 static inline int check_reg_index(int index) {
 	assert(index >= 0 && index < 8);
@@ -68,10 +174,13 @@ static inline int check_sreg_index(int index) {
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
+#define reg_s(index) (cpu.sr[check_sreg_index(index)])
 
 extern const char* regsl[];
 extern const char* regsw[];
 extern const char* regsb[];
 extern const char* regss[];
+
+void sreg_load();
 
 #endif
--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -30,19 +30,39 @@ char asm_buf[128];
 
 /* Used with exception handling. */
 jmp_buf jbuf;
-
+static inline void push (int val)
+{
+	current_sreg = R_SS;
+	reg_l (R_ESP) -= 4;
+	swaddr_write (reg_l (R_ESP) , 4 , val);
+}
 void raise_intr(uint8_t NO) {
 	/* TODO: Trigger an interrupt/exception with ``NO''.
 	 * That is, use ``NO'' to index the IDT.
 	 */
-    
-	
-
-
-    	
+    	Assert(NO * 8 <= cpu.idtr.seg_limit, "Interrupt number exceeded");
+    	GATE_descriptor gate;
+	idt_des = &gate;
+   	lnaddr_t pidt = cpu.idtr.base_addr + NO * 8;
+   	idt_des->first_part = lnaddr_read(pidt, 4);
+	idt_des->second_part = lnaddr_read(pidt + 4, 4);
+	Assert ((NO << 3) <= cpu.idtr.seg_limit,"idt out limit %hd, %d", (NO<<3), cpu.idtr.seg_limit);
+	push (cpu.eflags);
+	if (cpu.cr0.protect_enable == 0)
+	{
+		cpu.IF = 0;
+		cpu.TF = 0;
+	}
+	push (cpu.cs.selector);
+	push (cpu.eip); 
+    	cpu.cs.selector = idt_des -> segment;
+    	Assert(((cpu.cs.selector>>3)<<3) <= cpu.gdtr.seg_limit, "segment out limit %d, %d", ((cpu.cs.selector>>3)<<3), cpu.gdtr.seg_limit);
+	current_sreg = R_CS;
+    	sreg_load();
+    	cpu.eip = cpu.cs.seg_base + idt_des -> offset_15_0 + (idt_des -> offset_31_16 << 16);
+    /* Jump back to cpu_exec() */
     	longjmp(jbuf, 1);
 }
-
 void print_bin_instr(swaddr_t eip, int len) {
 	int i;
 	int l = sprintf(asm_buf, "%8x:   ", eip);
