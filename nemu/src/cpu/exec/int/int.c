5cad475002295a3c5030a74d6ab750b767ee69f0
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 20:39:00 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:39:00 up 10:22,  1 user,  load average: 1.50, 1.38, 1.14 500a12f2a5d04de7f6ebb031f8a9cb47648d52c1

--- nemu/src/cpu/exec/int/int.c
+++ nemu/src/cpu/exec/int/int.c
@@ -2,7 +2,10 @@
 #include <nemu.h>
 void raise_intr(uint8_t);
 make_helper(intr) {
-
+	int NO = instr_fetch(eip + 1, 1);
 	cpu.eip += 2;
+	print_asm("int %x",NO);
+    printf("1\n");
+	raise_intr (NO);
 	return 0;
 }
