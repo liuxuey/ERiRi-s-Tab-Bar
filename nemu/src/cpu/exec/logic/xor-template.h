c9622d8489bf60c93073cdaa4b8bc36f08446a96
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 23:07:04 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:07:04 up  1:13,  1 user,  load average: 1.02, 0.72, 0.61 afb1b4e45f8d669f8531de25d02f10e048dfdd4e

--- nemu/src/cpu/exec/logic/xor-template.h
+++ nemu/src/cpu/exec/logic/xor-template.h
@@ -8,7 +8,6 @@ static void do_execute () {
 	OPERAND_W(op_dest, result);
 	cpu.CF = cpu.OF = 0;
 	concat(updateCPU_, SUFFIX) (result);
-	printf("%d\n",cpu.eax);
 	/* TODO: Update EFLAGS. */
 	//panic("please implement me");
 	print_asm_template2();
