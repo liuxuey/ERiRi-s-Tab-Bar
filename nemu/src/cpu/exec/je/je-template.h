671f6a0d8c7d68f51b054cdc198d3641f8ffff6f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 10:20:18 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:20:18 up 3 min,  1 user,  load average: 2.00, 2.31, 1.07 146ed00a18a21f0e9555a692246b63b87ef70776

--- nemu/src/cpu/exec/je/je-template.h
+++ nemu/src/cpu/exec/je/je-template.h
@@ -7,6 +7,7 @@ static void do_execute() {
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("je %x",cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.ZF == 1)cpu.eip +=displacement;
+	print_asm_template1();
 }
 make_instr_helper(i)
 
