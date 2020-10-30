3677c74d07f66f4cb42a0265cca5cef299bec29a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-31 01:13:44 +0800

    >  compile NEMU 3019209351 lxy Linux lxy-Legion-Y7000-2019-PG0 5.4.0-52-generic #57-Ubuntu SMP Thu Oct 15 10:57:00 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:13:44 up  3:19,  1 user,  load average: 0.81, 0.55, 0.50 886a7674cb83a944f340b6aae05e697ab9edb553

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -1,30 +1,24 @@
+
 #include "cpu/exec/template-start.h"
 
 #define instr cmp
 
-static void do_execute() {
-	DATA_TYPE result = op_dest->val - op_src->val;
+void do_execute() {
+	DATA_TYPE ans = op_dest->val - op_src->val;
+	concat(updateCPU_, SUFFIX) (ans);
 	int len = (DATA_BYTE << 3) - 1;
 	cpu.CF = op_dest->val < op_src->val;
-	cpu.SF=result >> len;
-    	int s1,s2;
-	s1=op_dest->val>>len;
-	s2=op_src->val>>len;
-    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
-    	cpu.ZF=!result;
-	result ^= result >>4;
-	result ^= result >>2;
-	result ^= result >>1;
-	cpu.PF=!(result & 1);
+	cpu.OF = ((op_dest->val >> len) != (op_src->val >> len) && (op_src->val >> len) == cpu.SF);
+	print_asm_template2();
 }
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(si2rm)
-#endif
 
-make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
+make_instr_helper(i2a)
 
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
 
 #include "cpu/exec/template-end.h"
